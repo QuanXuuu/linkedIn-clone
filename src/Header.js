6eb17f17e169d3f2896e8b162a6d71d745626e36
Quan Xu <xuquan69220@gmail.com>
2022-09-01 19:37:34 +0200

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+
+function Header() {
+  return (
+    <div className="header">
+
+    
+    <div className="header__left">
+    <img src="https://images.unsplash.com/photo-1611944212129-29977ae1398c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGlua2VkaW58ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60" alt=""/>
+
+    <div className="header__search">
+        <SearchIcon />
+        <input type="text"/>
+    </div>
+    </div>
+
+    <div className="header__right">
+      <HeaderOption Icon={HomeIcon} title='Home'/>
+      <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+      <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+      <HeaderOption Icon={ChatIcon} title='Messaging'/>
+      <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+      <HeaderOption avatar="" title='me'/>
+    </div>
+    </div>
+  )
+}
+
+export default Header
