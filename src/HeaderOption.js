4d309f2c2782e18e325a7afc4796f8a08a368834
Quan Xu <xuquan69220@gmail.com>
2022-09-07 12:22:35 +0200

    add extension

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import './HeaderOption.css'
-import {Avatar} from '@mui/material'
-import { useSelector } from 'react-redux'
-import {selectUser} from './features/counter/userSlice'
+import React from "react";
+import "./HeaderOption.css";
+import { Avatar } from "@mui/material";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/counter/userSlice";
 
-
-function HeaderOption({Icon, title, onClick}) {
- const user  = useSelector(selectUser);
+function HeaderOption({ Icon, title, onClick }) {
+  const user = useSelector(selectUser);
 
   return (
-    <div onClick={onClick} className='headerOption'>
-        {Icon && <Icon className="headerOption__icon"/>}
-      
-          <Avatar className="headerOption__icon" src={user?.photoUrl}>
-            {user?.email[0]}
-          </Avatar>
-       
-        <h3 className="headerOption__title">{title}</h3>
+    <div onClick={onClick} className="headerOption">
+      {Icon && <Icon className="headerOption__icon" />}
+
+      <Avatar className="headerOption__icon" src={user?.photoUrl}>
+        {user?.email[0]}
+      </Avatar>
+
+      <h3 className="headerOption__title">{title}</h3>
     </div>
-  )
+  );
 }
 
-export default HeaderOption
+export default HeaderOption;
